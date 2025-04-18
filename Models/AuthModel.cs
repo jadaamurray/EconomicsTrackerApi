6e01498ff38aac23bb951382ad46d2d992b02a82
Jada Murray <jada736@icloud.com>
2025-04-18 17:21:50 +0100

    Adding first and last names, changing controllers and context and models and adding dtos to reflet

--- Models/AuthModel.cs
+++ Models/AuthModel.cs
@@ -4,5 +4,8 @@ namespace EconomicsTrackerApi.Models
     {
         public required string Email { get; set; }
         public required string Password { get; set; }
+
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
     }
 }
