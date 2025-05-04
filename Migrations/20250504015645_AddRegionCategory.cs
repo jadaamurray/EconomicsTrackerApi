457fb5fe041da8691873ad27881dc2e67b872ce6
Jada Murray <jada736@icloud.com>
2025-05-04 23:43:37 +0100

    Adding region category, database update

---
+++ Migrations/20250504015645_AddRegionCategory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EconomicsTrackerApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRegionCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Regions",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Regions");
+        }
+    }
+}
--- Models/Region.cs
+++ Models/Region.cs
@@ -10,10 +10,15 @@ namespace EconomicsTrackerApi.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // auto generate and increment id
         public int RegionId { get; set; }
+
         [StringLength(200, MinimumLength = 3)]
         public required string RegionName { get; set; }
+
         [StringLength(450, MinimumLength = 3)]
         public required string Description { get; set; }
+
+        [StringLength(100, MinimumLength = 3)]
+        public required string Category { get; set; }
 
         [JsonIgnore]
         public List<Data>? RegionDataPoints { get; set; } // nullable as a region can have 0 or many economic data points associated with it
